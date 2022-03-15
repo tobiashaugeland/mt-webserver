a422e5fb837ad95da712dbdca6dd48bbde960d25
tobiashaugeland <tobias.haugeland@gmail.com>
2022-03-15 14:22:40 +0100

    some more sem stuff

--- sem.c
+++ sem.c
@@ -1,27 +1,35 @@
 #include "sem.h"
+#include <stdlib.h>
+#include <pthread.h>
 
 struct SEM
 {
     unsigned int count;
+    pthread_cond_t cond;
+    pthread_mutex_t mutex;
 };
 
 SEM *sem_init(int init_val)
 {
     struct SEM *sem = malloc(sizeof(struct SEM));
     sem->count = init_val;
+    pthread_mutex_init(&sem->mutex, NULL);
+    pthread_cond_init(&sem->cond, NULL);
     return sem;
 }
 
 int sem_del(SEM *sem)
 {
+    pthread_mutex_destroy(&sem->mutex);
+    pthread_cond_destroy(&sem->cond);
     free(sem);
     return 0;
 }
 
 void P(SEM *sem)
 {
-    while ((*sem).count == 0);
-    sem->count--;
+    pthread_mutex_lock(&sem->mutex);
+    pthread_cond_wait()
 }
 
 void V(SEM *sem){
