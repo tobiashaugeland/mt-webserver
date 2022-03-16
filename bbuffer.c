0473e5be85f343586f75f264ab7ec9c10f41498c
Henrik Løvhaug <henrik.lovhaug@gmail.com>
2022-03-16 08:06:45 +0100

    Start of ring buffer

--- bbuffer.c
+++ bbuffer.c
@@ -7,6 +7,7 @@ struct BNDBUF{
     int remove;
     SEM *full;
     SEM *empty;
+    int size;
     int buffer[];
 };
 
@@ -16,5 +17,28 @@ BNDBUF * bb_init(unsigned int size){
     bbuffer -> remove = 0;
     bbuffer -> full = sem_init(0);
     bbuffer -> empty = sem_init(size);
+    bbuffer -> size = size;
 
+}
+
+void bb_del(BNDBUF *bb){
+    sem_del(bb->full);
+    sem_del(bb->empty);
+    free(bb);
+}
+
+int bb_get(BNDBUF *bb){
+    int item;
+    P(bb->empty);
+    item = bb->buffer[bb->remove];
+    bb->remove = (bb->remove + 1) % bb->size;
+    V(bb->full);
+    return item;
+}
+
+void bb_add(BNDBUF *bb, int fd){
+    P(bb->full);
+    bb->buffer[bb->insert] = fd;
+    bb->insert = (bb->insert + 1) % bb->size;
+    V(bb->empty);
 }
